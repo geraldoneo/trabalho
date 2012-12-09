15b5ff2dcebf9f35ab1c6d894e7e74a6377c2a3b
GeraldoResende <geraldoresende@gmail.com>
2012-12-09 20:41:50 -0200

    Revert "Revert "PrimeiroCommit""
    This reverts commit a6eb8c9ac290e9d84e100b73fdb5b94997c987c7.

---
+++ Laboratorio de Praticas ageis (Tiago)/App_Code/ClientRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using emprestimoModel;
+
+namespace Emprestimo
+{
+ 
+    public class ClientRepository
+    {
+        private emprestimoEntities _ctxEmprestimo = new emprestimoEntities();
+
+        public IQueryable<cliente> FindAll()
+        {
+            return from cliente in this._ctxEmprestimo.clientes
+                   orderby cliente.Nome
+                   select cliente;
+        }
+    }
+}
---
+++ Laboratorio de Praticas ageis (Tiago)/App_Code/EquipamentoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using emprestimoModel;
+
+namespace Emprestimo
+{
+ 
+    public class EquipamentoRepository
+    {
+        private emprestimoEntities _ctxEmprestimo = new emprestimoEntities();
+
+        public IQueryable<equipamento> FindAll()
+        {
+            return from equipamento in this._ctxEmprestimo.equipamentoes
+                   orderby equipamento.NumeroSerie
+                   select equipamento;
+        }
+    }
+}
